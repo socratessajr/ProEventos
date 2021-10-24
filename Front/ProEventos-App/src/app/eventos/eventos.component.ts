b05427c8d78bce71ef41131e3e26d4a96206eb41
Sócrates Júnior <socratessajunior@gmail.com>
2021-10-24 14:58:54 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any;
+  public eventosFiltrados: any;
+  widthImg = 150;
+  marginImg = 2;
+  mostrarImagem = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,8 +40,11 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+    response => {
+      this.eventos = response;
+      this.eventosFiltrados = this.eventos;
+    },
+    error => console.log(error)
     );
   }
 }
