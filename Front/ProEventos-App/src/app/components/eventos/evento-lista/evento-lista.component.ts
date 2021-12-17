11cc3f67cb07ebe4111090ae479c10160c2bd373
Sócrates Júnior <socratessajunior@gmail.com>
2021-12-16 23:17:52 -0300

    Layout Responsivo + Forms

---
+++ Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Evento } from 'src/app/models/Evento';
+import { EventoService } from 'src/app/services/evento.service';
+
+@Component({
+  selector: 'app-evento-lista',
+  templateUrl: './evento-lista.component.html',
+  styleUrls: ['./evento-lista.component.scss']
+})
+export class EventoListaComponent implements OnInit {
+
+  modalRef?: BsModalRef;
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+  widthImg = 150;
+  marginImg = 2;
+  mostrarImagem = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: Evento) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    private router: Router) { }
+
+  public ngOnInit(): void {
+    this.spinner.show();
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.eventoService.getEventos().subscribe(
+      (_eventos: Evento[]) => {
+        this.eventos = _eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error => {
+        console.log(error);
+        this.spinner.hide();
+        this.toastr.error('Erro ao carregar os Eventos');
+      },
+      () => {
+        this.spinner.hide()
+      }
+    );
+  }
+
+  public openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  public confirm(): void {
+    this.toastr.success('Evento excluído com sucesso!');
+    this.modalRef?.hide();
+  }
+
+  public decline(): void {
+    this.modalRef?.hide();
+  }
+
+  public eventoDetail(id: number): void{
+    this.router.navigate([`eventos/detalhe/${id}`]);
+  }
+
+}
--- Front/ProEventos-App/src/app/shared/title/title.component.ts
+++ Front/ProEventos-App/src/app/shared/title/title.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-title',
@@ -7,10 +8,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
   @Input() title: string | undefined;
-  constructor() { }
+  @Input() subtitle = 'Desde 2011';
+  @Input() iconClass = 'fa fa-user';
+  @Input() btnList = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
     //this.title = text;
   }
 
+  list(): void{
+    this.router.navigate([`/${this.title?.toLocaleLowerCase()}/lista`])
+  }
 }
